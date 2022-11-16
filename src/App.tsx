6c4871b593e49a8abe4a1718ef74d7fb630493a8
Tùng Đường <>
2022-11-16 15:20:53 +0700

    Format file App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline text-slate-600">
-    Hello world!
-  </h1>
+        Hello world!
+      </h1>
     </div>
   );
 }
