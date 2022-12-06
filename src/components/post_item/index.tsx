9fb2fa7da8a99e8833a19da5ff399b2a136cd83f
Tùng Đường <>
2022-12-06 15:07:43 +0700

    Create Model, Fake Data for List Posts

--- src/components/post_item/index.tsx
+++ src/components/post_item/index.tsx
@@ -1,3 +1,6 @@
+import { currentUser } from "../../common/data/current_user";
+import { Post } from "../../common/models";
+import { StringUtils } from "../../utils";
 import {
   IconComment,
   IconEmoji,
@@ -9,19 +12,20 @@ import {
 import { IconApp } from "../icon_app";
 import UserAvatarStory from "../user_avatar_story";
 
-export default function PostItem(): JSX.Element {
+export default function PostItem({ post }: { post: Post }): JSX.Element {
+  const userNameOfAuthor = StringUtils.displayUserName(post.user.userName);
+
   return (
     <div className="bg-white mt-4 rounded-lg border-[1px]">
       {/* Header */}
       <div className="flex flex-row my-2 items-center pl-3">
         <UserAvatarStory
-          url="https://randomuser.me/api/portraits/men/32.jpg"
-          size={44}
+          url={post.user.avatar}
+          size={32}
           haveSeenBefore={false}
         />
         <div className="flex flex-col grow mx-2">
-          <p className="text-sm font-semibold">username</p>
-          <p className="text-sm">subtitle</p>
+          <p className="text-sm font-semibold">{userNameOfAuthor}</p>
         </div>
         <IconApp icon={<IconMore />} onClick={() => console.log("")} />
       </div>
@@ -29,7 +33,7 @@ export default function PostItem(): JSX.Element {
       <img
         draggable={false}
         className="max-h-[30rem] w-full"
-        src="https://images.unsplash.com/photo-1666245035270-42b01a0532f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=470&q=80"
+        src={post.image}
         alt=""
       />
 
@@ -51,44 +55,50 @@ export default function PostItem(): JSX.Element {
         1: Liked (Current User)
         2: Not yet
       */}
-      <div>
-        <p className="text-sm ml-3">
-          Like by <span className="font-semibold">brunode</span> and{" "}
-          <span className="font-semibold">1234 others</span>
-        </p>
+      <div className="ml-3">
+      {post.isLiked ? (
+        <div>
+          <p className="text-sm">
+            Like by{" "}
+            <span className="font-semibold">{StringUtils.displayUserName(currentUser.userName)}</span> and{" "}
+            <span className="font-semibold">{StringUtils.formatNumber(post.totalLike - 1)} others</span>
+          </p>
+        </div>
+      ) : (
+        <span className="text-sm font-semibold">{StringUtils.formatNumber(post.totalLike)} likes</span>
+      )}
       </div>
+      
 
       {/* Description */}
       <div className="mx-3 my-2">
         <p className="text-sm">
-          <span className="font-semibold">brunode</span> abcxyz
+          <span className="font-semibold">{userNameOfAuthor}</span> {post.description}
         </p>
       </div>
 
       {/* Total comments */}
       <div className="mx-3 my-2">
         <p className="text-sm font-medium text-secondary-text">
-          View all 1234 comments
+          View all {StringUtils.formatNumber(post.totalCmt)} comments
         </p>
       </div>
 
       {/* created time */}
-      <div className="mx-3 my-2 text-secondary-text text-[10px]">3 HOURS AGO</div>
+      <div className="mx-3 my-2 text-secondary-text text-[10px]">
+        3 HOURS AGO
+      </div>
 
       <div className="flex flex-row items-center border-t-[1px] border-t-post-separator mx-1 py-2">
         <IconApp icon={<IconEmoji />} onClick={() => {}} />
         <div className="grow">
-          <input 
+          <input
             type="text"
             placeholder="Add a comment..."
             className="w-full pl-1 pr-5 h-5 focus:outline-none focus:border-transparent"
           />
         </div>
-        <button
-          className="mr-3 text-primary-button font-semibold"
-        >
-          Post
-        </button>
+        <button className="mr-3 text-primary-button font-semibold">Post</button>
       </div>
     </div>
   );
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,16 @@
+import { listPosts } from "../../common/data/list_posts";
 import PostItem from "../../components/post_item";
 import UserStories from "../../components/user_stories";
 import styles from "./styles.module.css";
 
 export default function HomePage(): JSX.Element {
   return (
-    <div className="flex flex-row justify-center">
+    <div className="flex flex-row justify-center pb-16">
       <div className={`${styles.container_feed}`}>
         {/* List Stories */}
         <UserStories />
 
-        <PostItem />
+        {listPosts && listPosts.map((e) => <PostItem post={e} key={e.id}/>)}
       </div>
       <div className={`${styles.suggestions}`}>Div Right</div>
     </div>
