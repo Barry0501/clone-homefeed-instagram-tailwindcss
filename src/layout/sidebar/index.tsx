0e498c64ceb690b364d2193b5c181bb43c1aa945
Tùng Đường <>
2022-12-09 14:24:46 +0700

    Make UI List Suggestions

--- src/layout/sidebar/index.tsx
+++ src/layout/sidebar/index.tsx
@@ -1,3 +1,4 @@
+import { currentUser } from "../../common/data/current_user";
 import {
   IconCreate,
   IconCreateActive,
@@ -26,6 +27,8 @@ export default function SideBar({
   tabActive: string;
   onClickTab: (s: string) => void;
 }): JSX.Element {
+  const userAvatar = currentUser.avatar;
+
   return (
     <div className={`${styles.sidebar}`}>
       <div className={`cursor-pointer mb-2 mt-5 px-3 pt-5 pb-4`}>
@@ -43,53 +46,43 @@ export default function SideBar({
           icon={<IconSearch />}
           activeIcon={<IconSearchActive />}
           title="Search"
-          isActive={false}
+          isActive={tabActive === "search"}
           onClick={() => onClickTab("search")}
         />
         <NavItem
           icon={<IconExplore />}
           activeIcon={<IconExploreActive />}
-          isActive={tabActive === 'explore'}
+          isActive={tabActive === "explore"}
           title={"Explore"}
           onClick={() => onClickTab("explore")}
         />
         <NavItem
           icon={<IconMessages />}
           activeIcon={<IconMessagesActive />}
-          isActive={false}
+          isActive={tabActive === "messages"}
           title={"Messages"}
-          onClick={() => console.log("")}
+          onClick={() => onClickTab("messages")}
         />
         <div className={`${styles.hide_on_mobile}`}>
           <NavItem
             icon={<IconNofication />}
             activeIcon={<IconNoficationActive />}
-            isActive={false}
+            isActive={tabActive === "notifications"}
             title={"Notifications"}
-            onClick={() => console.log("")}
+            onClick={() => onClickTab("notifications")}
           />
         </div>
         <NavItem
           icon={<IconCreate />}
           activeIcon={<IconCreateActive />}
-          isActive={false}
-          title={"Create"}
-          onClick={() => console.log("")}
+          isActive={tabActive === "creates"}
+          title={"Creates"}
+          onClick={() => onClickTab("creates")}
         />
         <NavItem
-          icon={
-            <MenuAvatar
-              url="https://randomuser.me/api/portraits/men/32.jpg"
-              isActive={false}
-            />
-          }
-          activeIcon={
-            <MenuAvatar
-              url="https://randomuser.me/api/portraits/men/32.jpg"
-              isActive={true}
-            />
-          }
-          isActive={tabActive === 'profile'}
+          icon={<MenuAvatar url={userAvatar} isActive={false} />}
+          activeIcon={<MenuAvatar url={userAvatar} isActive={true} />}
+          isActive={tabActive === "profile"}
           title={"Profile"}
           onClick={() => onClickTab("profile")}
         />
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,8 @@
+import { currentUser } from "../../common/data/current_user";
 import { listPosts } from "../../common/data/list_posts";
+import { ButtonLink } from "../../components/button_link";
+import { ItemRow } from "../../components/item_row";
+import { ListSuggestion } from "../../components/list_suggestions";
 import PostItem from "../../components/post_item";
 import UserStories from "../../components/user_stories";
 import styles from "./styles.module.css";
@@ -10,9 +14,28 @@ export default function HomePage(): JSX.Element {
         {/* List Stories */}
         <UserStories />
 
-        {listPosts && listPosts.map((e) => <PostItem post={e} key={e.id}/>)}
+        {listPosts && listPosts.map((e) => <PostItem post={e} key={e.id} />)}
       </div>
-      <div className={`${styles.suggestions}`}>Div Right</div>
+      <div className={`${styles.suggestions}`}>
+        <ItemRow
+          url={currentUser.avatar}
+          title={currentUser.userName}
+          subTitle={currentUser.displayName}
+          rightItem={
+            <ButtonLink textBtn="Switch" onClick={() => console.log("")} />
+          }
+        />
+        <div className="flex flex-row justify-between">
+            <p className="font-bold text-secondary-text text-[14px]">
+              Suggestions For You
+            </p>
+            <button className="text-xs font-bold" >
+              See All
+            </button>
+        </div>
+
+        <ListSuggestion />
+      </div>
     </div>
   );
 }
